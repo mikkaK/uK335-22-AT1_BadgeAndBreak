f8c0c093421e8f343508b59880ad762d2c4fa04d
MikkaK <mikka.kummer@noseryoung.ch>
2022-12-15 11:29:00 +0100

    fixed router

--- App.tsx
+++ App.tsx
@@ -1,12 +1,43 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  Text,
+  MD3LightTheme as DefaultTheme,
+  Provider as PaperProvider,
+  IconButton,
+  MD3Colors,
+} from "react-native-paper";
+import Home from "./src/pages/Home";
+import Details from "./src/pages/Details";
+
+
+type RootStackParamList = {
+  Home: {};
+  Details: {};
+}
+
+const Stack = createStackNavigator<RootStackParamList>()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Jan isch en saftsack</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={DefaultTheme}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ title: "Home" }}
+          />
+          <Stack.Screen 
+          name="Details"
+          component={Details}
+          options={{ title: "Detail" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
 
@@ -18,3 +49,4 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
