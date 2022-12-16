c3390d8a942aa202d620c0b5366fb8d567b4e8d0
MikkaK <mikka.kummer@noseryoung.ch>
2022-12-16 09:04:44 +0100

    added WeekdayBar.tsx

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -48,7 +48,7 @@ export default function Details() {
     <CustomTimePicker handleConfirm={handleTimeChange} initialVisibility={false} handleTimeChange={handleTimeChange}/>
  </View>
  <View style={styles.container}>
-
+     
  </View>
  <View style={styles.container}>
 
