6c7d49af0885f6d5fa8529c85de9de793b34a140
Gianluca Daffre <gianluca.daffre@noseryoung.ch>
2022-12-21 16:15:05 +0100

    trying storage

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,148 +1,158 @@
-import {Card, Title, TouchableRipple} from 'react-native-paper';
-import {StatusBar} from 'expo-status-bar';
-import {ImageBackground, ScrollView, Text, View} from 'react-native';
-import {useEffect, useState} from 'react';
-import {ReminderType} from '../../types/models/Reminders.models';
-import weekDayEnum from '../../config/WeekDays';
-import repeatEnum from '../../config/Repeat';
-import AddNewReminderFAB from '../atoms/addNewReminderFAB';
-import {styles} from '../../styles/home.styles';
-import {useTranslation} from "react-i18next";
+import { Card, Title, TouchableRipple } from "react-native-paper";
+import { StatusBar } from "expo-status-bar";
+import { ImageBackground, ScrollView, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ReminderType } from "../../types/models/Reminders.models";
+import weekDayEnum from "../../config/WeekDays";
+import repeatEnum from "../../config/Repeat";
+import AddNewReminderFAB from "../atoms/addNewReminderFAB";
+import { styles } from "../../styles/home.styles";
+import { useTranslation } from "react-i18next";
 import StorageService from "../../services/StorageService";
 import SwitchButton from "../atoms/toggleSwitch";
 
+export default function Home({ navigation }) {
+  const { t } = useTranslation();
+  const { storeData, getData } = StorageService;
+  const [reminders, setReminders] = useState<ReminderType[]>([
+    {
+      id: 1,
+      title: "Das ist ein Test",
+      time: "12:00",
+      days: [{ value: weekDayEnum.MONDAY, isSelected: true }],
+      repeat: repeatEnum.DAILY_REPEAT,
+      isActive: true,
+    },
+    {
+      id: 2,
+      title: "Das ist noch ein Test",
+      time: "13:00",
+      days: [{ value: weekDayEnum.MONDAY, isSelected: true }],
+      repeat: repeatEnum.WEEKLY_REPEAT,
+      isActive: false,
+    },
+    {
+      id: 3,
+      title: "Das ist ein Test",
+      time: "12:00",
+      days: [{ value: weekDayEnum.MONDAY, isSelected: true }],
+      repeat: repeatEnum.DAILY_REPEAT,
+      isActive: true,
+    },
+    {
+      id: 4,
+      title: "Das ist noch ein Test",
+      time: "13:00",
+      days: [{ value: weekDayEnum.MONDAY, isSelected: true }],
+      repeat: repeatEnum.WEEKLY_REPEAT,
+      isActive: false,
+    },
+    {
+      id: 5,
+      title: "Das ist ein Test",
+      time: "12:00",
+      days: [{ value: weekDayEnum.MONDAY, isSelected: true }],
+      repeat: repeatEnum.DAILY_REPEAT,
+      isActive: true,
+    },
+    {
+      id: 6,
+      title: "Das ist noch ein Test",
+      time: "13:00",
+      days: [{ value: weekDayEnum.MONDAY, isSelected: true }],
+      repeat: repeatEnum.WEEKLY_REPEAT,
+      isActive: false,
+    },
+    {
+      id: 7,
+      title: "Das ist ein Test",
+      time: "12:00",
+      days: [{ value: weekDayEnum.MONDAY, isSelected: true }],
+      repeat: repeatEnum.DAILY_REPEAT,
+      isActive: true,
+    },
+    {
+      id: 82,
+      title: "Das ist noch ein Test",
+      time: "13:00",
+      days: [{ value: weekDayEnum.MONDAY, isSelected: true }],
+      repeat: repeatEnum.WEEKLY_REPEAT,
+      isActive: false,
+    },
+    {
+      id: 6,
+      title: "Das ist noch ein Test",
+      time: "13:00",
+      days: [{ value: weekDayEnum.MONDAY, isSelected: true }],
+      repeat: repeatEnum.WEEKLY_REPEAT,
+      isActive: false,
+    },
+    {
+      id: 7,
+      title: "Das ist ein Test",
+      time: "12:00",
+      days: [{ value: weekDayEnum.MONDAY, isSelected: true }],
+      repeat: repeatEnum.DAILY_REPEAT,
+      isActive: true,
+    },
+    {
+      id: 81,
+      title: "Das ist noch ein Test",
+      time: "13:00",
+      days: [{ value: weekDayEnum.MONDAY, isSelected: true }],
+      repeat: repeatEnum.WEEKLY_REPEAT,
+      isActive: false,
+    },
+    {
+      id: 6,
+      title: "Das ist noch ein Test",
+      time: "13:00",
+      days: [{ value: weekDayEnum.MONDAY, isSelected: true }],
+      repeat: repeatEnum.WEEKLY_REPEAT,
+      isActive: false,
+    },
+  ]);
 
-export default function Home({navigation}) {
-    const {t} = useTranslation()
-    const {storeData, getData} = StorageService;
-    const [reminders, setRemiders] = useState<ReminderType[]>([
-        {
-            id: 1,
-            title: "Das ist ein Test",
-            time: "12:00",
-            days: [{value:weekDayEnum.MONDAY,isSelected:true}],
-            repeat: repeatEnum.DAILY_REPEAT,
-            isActive: true,
-        }, {
-            id: 2,
-            title: "Das ist noch ein Test",
-            time: "13:00",
-            days: [{value:weekDayEnum.MONDAY,isSelected:true}],
-            repeat: repeatEnum.WEEKLY_REPEAT,
-            isActive: false
-        },
-        {
-            id: 3,
-            title: "Das ist ein Test",
-            time: "12:00",
-            days: [{value:weekDayEnum.MONDAY,isSelected:true}],
-            repeat: repeatEnum.DAILY_REPEAT,
-            isActive: true
-        }, {
-            id: 4,
-            title: "Das ist noch ein Test",
-            time: "13:00",
-            days: [{value:weekDayEnum.MONDAY,isSelected:true}],
-            repeat: repeatEnum.WEEKLY_REPEAT,
-            isActive: false
-        },
-        {
-            id: 5,
-            title: "Das ist ein Test",
-            time: "12:00",
-            days: [{value:weekDayEnum.MONDAY,isSelected:true}],
-            repeat: repeatEnum.DAILY_REPEAT,
-            isActive: true
-        }, {
-            id: 6,
-            title: "Das ist noch ein Test",
-            time: "13:00",
-            days: [{value:weekDayEnum.MONDAY,isSelected:true}],
-            repeat: repeatEnum.WEEKLY_REPEAT,
-            isActive: false
-        },
-        {
-            id: 7,
-            title: "Das ist ein Test",
-            time: "12:00",
-            days: [{value:weekDayEnum.MONDAY,isSelected:true}],
-            repeat: repeatEnum.DAILY_REPEAT,
-            isActive: true
-        }, {
-            id: 82,
-            title: "Das ist noch ein Test",
-            time: "13:00",
-            days: [{value:weekDayEnum.MONDAY,isSelected:true}],
-            repeat: repeatEnum.WEEKLY_REPEAT,
-            isActive: false
-        }
-        , {
-            id: 6,
-            title: "Das ist noch ein Test",
-            time: "13:00",
-            days: [{value:weekDayEnum.MONDAY,isSelected:true}],
-            repeat: repeatEnum.WEEKLY_REPEAT,
-            isActive: false
-        },
-        {
-            id: 7,
-            title: "Das ist ein Test",
-            time: "12:00",
-            days: [{value:weekDayEnum.MONDAY,isSelected:true}],
-            repeat: repeatEnum.DAILY_REPEAT,
-            isActive: true
-        }, {
-            id: 81,
-            title: "Das ist noch ein Test",
-            time: "13:00",
-            days: [{value:weekDayEnum.MONDAY,isSelected:true}],
-            repeat: repeatEnum.WEEKLY_REPEAT,
-            isActive: false
-        }
-        , {
-            id: 6,
-            title: "Das ist noch ein Test",
-            time: "13:00",
-            days: [{value:weekDayEnum.MONDAY,isSelected:true}],
-            repeat: repeatEnum.WEEKLY_REPEAT,
-            isActive: false
-        }
-    ])
+  useEffect(() => {
+    if (reminders.length === 0) {
+      setReminders([]);
+    } else {
+      storeData("@mykey", JSON.stringify(reminders));
+      getData("@mykey").then((value) => {
+        console.log("Values in Storage:", JSON.parse(value));
+      });
+    }
+  }, []);
 
-    useEffect(()=>{
-        if(reminders.length === 0){
-            setRemiders([])
-        }else {
-            storeData("allReminders", reminders.toString())
-        }
-    }, [])
-
-    return (
-
-        <View style={styles.container}>
-            <ImageBackground source={require('./../../../assets/background.png')}
-                             style={{width: '100%', height: '100%'}}>
-                <ScrollView style={styles.scrollView}>
-                        {reminders.map(reminder => (
-                            <TouchableRipple
-                                onPress={() =>
-                                    navigation.navigate("Details",{reminder,reminders})}>
-                            <Card style={styles.card}>
-                                <Card.Content>
-                                    <Title>{reminder.id}</Title>
-                                    <Text>{t("description.time")}, {reminder.time}, {t("description." + reminder.repeat)}</Text>
-                                </Card.Content>
-                                <SwitchButton/>
-                            </Card>
-                            </TouchableRipple>
-                        ))}
-                </ScrollView>
-                <StatusBar style="auto"/>
-                <View style={styles.FABContainer}>
-                <AddNewReminderFAB navigation={navigation} reminders={reminders}/>
-                </View>
-            </ImageBackground>
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("./../../../assets/background.png")}
+        style={{ width: "100%", height: "100%" }}>
+        <ScrollView style={styles.scrollView}>
+          {reminders.map((reminder) => (
+            <TouchableRipple
+              onPress={() =>
+                navigation.navigate("Details", { reminder, reminders })
+              }>
+              <Card style={styles.card}>
+                <Card.Content>
+                  <Title>{reminder.id}</Title>
+                  <Text>
+                    {t("description.time")}, {reminder.time},{" "}
+                    {t("description." + reminder.repeat)}
+                  </Text>
+                </Card.Content>
+                <SwitchButton />
+              </Card>
+            </TouchableRipple>
+          ))}
+        </ScrollView>
+        <StatusBar style="auto" />
+        <View style={styles.FABContainer}>
+          <AddNewReminderFAB navigation={navigation} reminders={reminders} />
         </View>
-
-    )
+      </ImageBackground>
+    </View>
+  );
 }
