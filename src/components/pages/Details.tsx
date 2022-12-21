ad9cd831f7c40ee1233076b548d4dbc5a0a18a3d
MikkaK <mikka.kummer@noseryoung.ch>
2022-12-21 09:50:04 +0100

    merge commit

--- src/components/pages/Details.tsx
+++ src/components/pages/Details.tsx
@@ -1,15 +1,12 @@
 import {useCallback, useEffect, useState} from "react";
-import {KeyboardAvoidingView, StyleSheet, Text, View} from "react-native";
+import {StyleSheet, Text, View} from "react-native";
 import {Button, Snackbar, TextInput, useTheme} from 'react-native-paper';
 import CustomTimePicker from "../atoms/CustomTimePicker";
 import WeekdayBar from "../atoms/WeekdayBar";
 import RepeatBar from "../atoms/RepeatBar";
 import {ReminderType} from "../../types/models/Reminders.models";
 import StorageService from "../../services/StorageService";
-import weekDayEnum from "../../config/WeekDays";
 import {WeekdayType} from "../../types/WeekDayType";
-import Repeat from "../../config/Repeat";
-import useThenable from "@react-navigation/native/lib/typescript/src/useThenable";
 
 
 export default function Details() {
@@ -19,8 +16,10 @@ export default function Details() {
     const [selectedReminder, setSelectedReminder] = useState<ReminderType>()
     const {storeData, getData} = StorageService
     const [enteredTime, setEnteredTime] = useState<string>();
-    const [selectedDays, setSelectedDays ] = useState<WeekdayType[]> ([])
+    const [selectedDays, setSelectedDays] = useState<WeekdayType[]>([])
     const [selectedRepeat, setSelectedRepeat] = useState<string>("never")
+    const [errorText, setErrorText] = useState<string>("An undefined error occurred")
+    const [allReminders, setAllReminders] = useState<ReminderType[]>()
     const styles = StyleSheet.create({
         container: {
             flex: 1,
@@ -65,53 +64,57 @@ export default function Details() {
             }
             //todo set fields to previous values when in "edit mode"
             setSelectedReminder(JSON.parse(value))
+        getData("allReminders").then((value) => {
+            setAllReminders(JSON.parse(value))
+        })
         })
     }, [])
 
     const handleSave = () => {
-        let allReminders:ReminderType[] = [];
-        getData("allReminders").then((value) => {
-            console.log(value + "AllReminders")
-                allReminders = JSON.parse(value);
-        })
-        const tempReminder: ReminderType = {
-            days: selectedDays,
-            repeat: selectedRepeat,
-            isActive: true,
-            id: 1,
-            time: enteredTime,
-            title: enteredText
+        if (selectedDays.length && enteredText && enteredTime) {
+            setIsSnackbarVisible(false)
+            const tempReminder: ReminderType = {
+                days: selectedDays,
+                repeat: selectedRepeat,
+                isActive: true,
+                id: 1,
+                time: enteredTime,
+                title: enteredText
+            }
+            console.log(tempReminder);
+
+            console.log(allReminders);
+            allReminders.push(tempReminder);
+            //todo push to correct storage (get correct storage in first term)
+            storeData("allReminders", allReminders.toString()).then(() => {
+                console.log("successfully pushed to reminders")
+            })
+        } else {
+            setErrorText("Please fill out every field")
+            setIsSnackbarVisible(true)
         }
-        console.log(tempReminder);
-        //todo error handling and validity checks
-        console.log(allReminders);
-        allReminders.push(tempReminder);
-        //todo push to correct storage (get correct storage in first line)
-        storeData("allReminders", allReminders.toString()).then(() => {
-            console.log("successfully pushed to reminders")
-        });
     }
 
     const handleTimeConfirm = useCallback(({hours, minutes}) => {
         let hourString = hours.toString();
         let minuteString = minutes.toString();
-        setEnteredTime(hourString+":"+minuteString);
-        },[enteredTime])//maybe remove input
+        setEnteredTime(hourString + ":" + minuteString);
+    }, [enteredTime])//maybe remove input
 
-    const handleWeekdayPress = useCallback((item:WeekdayType) => {
+    const handleWeekdayPress = useCallback((item: WeekdayType) => {
         let selectedDaysCopy = [...selectedDays];
-        const { isSelected, value } = item;
-       if (isSelected){
-           selectedDaysCopy.push(item)
-       }else{
-           selectedDaysCopy = selectedDaysCopy.filter(day => day.value !== value)
-       }
+        const {isSelected, value} = item;
+        if (isSelected) {
+            selectedDaysCopy.push(item)
+        } else {
+            selectedDaysCopy = selectedDaysCopy.filter(day => day.value !== value)
+        }
         setSelectedDays(selectedDaysCopy);
     }, [selectedDays])
 
     const handleRepeatChange = useCallback((value) => {
         setSelectedRepeat(value)
-    },[selectedRepeat])
+    }, [selectedRepeat])
 
     return (
         <>
@@ -137,7 +140,9 @@ export default function Details() {
                 <RepeatBar handleChange={handleRepeatChange}/>
             </View>
             <View style={[styles.container, styles.saveContainer]}>
-                <Button mode={"contained"} style={styles.saveButton} onPress={handleSave}>
+                <Button mode={"contained"} style={styles.saveButton} onPress={() => {
+                    handleSave();
+                }}>
                     <Text style={{width: "90%"}}>Save</Text>
                 </Button>
             </View>
@@ -149,7 +154,7 @@ export default function Details() {
                     }}
                     style={{backgroundColor: theme.colors.error}}
                 >
-                    <Text style={{color: theme.colors.onError}}>Error</Text>
+                    <Text style={{color: theme.colors.onError}}>{errorText}</Text>
                 </Snackbar>
             </View>
         </>);
